6e4fbab292c0d7b6e4a1ad2e7bab17b6d78cee7b
v.volodkovych <volodkovichv88@gmail.com>
2023-08-27 20:27:15 +0300

    1w

--- sendbox/src/main/java/ua/qa/pft/sendbox/main.java
+++ sendbox/src/main/java/ua/qa/pft/sendbox/main.java
@@ -3,6 +3,6 @@ package ua.qa.pft.sendbox;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("1Hello, world!");
     }
 }
